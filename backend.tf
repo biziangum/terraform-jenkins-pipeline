1d57753cd78d54a65d6b960cc7b8624b2e5f0b0c
culates <ngumisem@gmail.com>
2021-07-22 20:43:22 -0500

    changes in dir

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "tf-state-managements-system"
     key = "jt.tfstate"
     region = "us-west-2"
     dynamodb_table = "tfstate"
